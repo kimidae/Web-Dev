6b989b6843e2894c7670ac9d2408a3b297fb11dc
kimidae <157453082+kimidae@users.noreply.github.com>
2025-02-15 14:55:10 +0500

    Add files via upload

---
+++ lab3/JS tasks/data types/2.js
@@ -0,0 +1,31 @@
+function ucFirst(str) {
+    if (!str) return str;
+  
+    return str[0].toUpperCase() + str.slice(1);
+  }
+  
+  alert( ucFirst("john") ); 
+  
+  // --2
+
+  function checkSpam(str) {
+    let lowerStr = str.toLowerCase();
+  
+    return lowerStr.includes('book') || lowerStr.includes('xxx');
+  }
+  
+  alert( checkSpam('buy bOOk now') );
+  alert( checkSpam('free xxxxx') );
+  alert( checkSpam("innocent rabbit") );
+  
+  // --3
+
+  function truncate(str, maxlength) {
+    return (str.length > maxlength) ?
+      str.slice(0, maxlength - 1) + '…' : str;
+  }
+  // --4
+  
+  function extractCurrencyValue(str) {
+    return +str.slice(1);
+  }
